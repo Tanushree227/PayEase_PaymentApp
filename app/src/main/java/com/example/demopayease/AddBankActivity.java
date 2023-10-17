d84bda23a9a8e5b2525bc89acd83fdf14be7722d
Tanushree227 <tanushreedas813@gmail.com>
2023-10-17 10:13:22 +0530

    added new activity i.e. AddBankActivity.java

---
+++ app/src/main/java/com/example/demopayease/AddBankActivity.java
@@ -0,0 +1,14 @@
+package com.example.demopayease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddBankActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_bank);
+    }
+}
--- app/src/main/java/com/example/demopayease/ProfilePageActivity.java
+++ app/src/main/java/com/example/demopayease/ProfilePageActivity.java
@@ -2,9 +2,11 @@ package com.example.demopayease;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,6 +14,8 @@ import android.widget.Toast;
 public class ProfilePageActivity extends AppCompatActivity {
 
     TextView userText, emailText, phoneText;
+
+    Button addBankbtn;
 
     DBHandler dbHandler;
 
@@ -24,6 +28,15 @@ public class ProfilePageActivity extends AppCompatActivity {
         userText = findViewById(R.id.userText);
         emailText = findViewById(R.id.emailText);
         phoneText = findViewById(R.id.phoneText);
+        addBankbtn = findViewById(R.id.addBankbtn);
+
+        addBankbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(ProfilePageActivity.this, AddBankActivity.class);
+                startActivity(i1);
+            }
+        });
 
         ImageButton backP = (ImageButton) findViewById(R.id.backprofilebtn);
         backP.setOnClickListener(new View.OnClickListener() {
